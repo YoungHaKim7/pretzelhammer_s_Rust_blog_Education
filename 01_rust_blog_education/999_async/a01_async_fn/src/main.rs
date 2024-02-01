5fe6bc420c262ff5d6e3f10a72f54abd25e19948
GYoung <ytok1108@aol.com>
2024-02-02 00:09:33 +0900

    Trials, Traits and Tribulations

--- 01_rust_blog_education/999_async/a01_async_fn/src/main.rs
+++ 01_rust_blog_education/999_async/a01_async_fn/src/main.rs
@@ -3,12 +3,29 @@ use std::{
     fmt::{write, Display},
     fs,
     path::PathBuf,
+    rc::Rc,
     sync::Arc,
 };
 
 // struct Headers(HashMap<String, String>);
 struct HeaderError;
 struct FileName(String);
+
+struct AdhocStrategy {
+    task: AdhocTask,
+}
+
+impl TryFrom<Rc<HandlerEvent>> for AdhocStrategy {
+    type Error = MyError;
+
+    fn try_from(value: Rc<HandlerEvent>) -> Result<Self, Self::Error> {
+        let task = match event.body {
+            Some(ref body) => serde_json::from_str::<AdhocTask>(body.as_str()),
+            None => return Err(HeaderError.into()),
+        }?;
+        Ok(Self { task })
+    }
+}
 
 impl Display for HeaderError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -18,9 +35,50 @@ impl Display for HeaderError {
 
 impl std::error::Error for HeaderError {}
 
+impl EventStrategy for AdhocStrategy {
+    fn file_name(&self) -> Result<FileName, MyError> {
+        Ok(FileName::default())
+    }
+
+    fn code(&self) -> Result<String, MyError> {
+        Ok(self.task.code.clone())
+    }
+
+    fn payload(&self) -> Option<Value> {
+        self.task.payload.clone()
+    }
+
+    fn health_check(&self, _: &Worker) -> Result<HandlerResult, MyError> {
+        Ok(HandlerResult::default())
+    }
+
+    fn parse_result(&self, worker_result: WorkerResult) -> Result<HandlerResult, MyError> {
+        Ok(HandlerResult {
+            status_code: if worker_result.error.is_some() {
+                StatusCode::INTERNAL_SERVER_ERROR.as_u16()
+            } else {
+                StatusCode::OK.as_u16()
+            },
+            headers: Some(HashMap::from([(
+                CONTENT_TYPE.to_string(),
+                "application/json; charset=utf-8".into(),
+            )])),
+            body: Some(serde_json::to_string(&worker_result)),
+        })
+    }
+}
+
 trait Headers {
     fn get_required_header(&self, name: &str) -> Result<String, MyError>;
     fn get_optional_header(&self, name: &str) -> Option<String>;
+}
+
+trait EventStrategy {
+    fn file_name() -> FileName;
+    fn code() -> Result<String, MyError>;
+    fn payload() -> Option<Value>;
+    fn health_check() -> Result<HandlerResult, MyError>;
+    fn parse_result(result: WorkerResult) -> Result<HeadlerResult, MyError>;
 }
 
 impl Headers for HashMap<String, String> {
@@ -204,35 +262,35 @@ async fn main() -> Result<HandlerResult, Box<dyn std::error::Error + Send + Sync
         serde_json::from_str(&body).unwrap_or(Some(serde_json::Value::String(body)))
     });
 
-    let worker_result = worker.execute(file_name, code, payload, config).await?
+    let worker_result = worker.execute(file_name, code, payload, config).await?;
     if worker_result.error.is_some() {
         HandlerResult {
-            status_code: StatusCode:: INTERNAL_SERVER_ERROR.as_u16(),
+            status_code: StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
             headers: None,
             body: None,
         }
     } else if let Some(result) = worker_result.result {
-            HandlerResult {
-                status_code: StatusCode::OK.as_u16(),
-                headers: Some(HashMap::form([(
-                        CONTENT_TYPE.to_string(),
-                        if result.is_object() || result.is_array() {
-                            "apllication/json; charset=utf-8".into()
-                        }else {
-                            "text/plain".into()
-                        },
-                    )])),
-                    body: Some(match result {
-                        serde_json::Value::Null => "null".to_string(),
-                        serde_json::Value::Bool(v) => v.to_string(),
-                        serde_json::Value::Number(ref v)=> format!("{}", v),
-                        serde_json::Value::String(ref v)=> v.to_owned(),
-                        serde_json::Value::Array(_)=> result.to_string(),
-                        serde_json::Value::Object(_)=> result.to_string(),
-                    }),
-                }
-        } else {
-        HandlerResult::default()
+        HandlerResult {
+            status_code: StatusCode::OK.as_u16(),
+            headers: Some(HashMap::form([(
+                CONTENT_TYPE.to_string(),
+                if result.is_object() || result.is_array() {
+                    "apllication/json; charset=utf-8".into()
+                } else {
+                    "text/plain".into()
+                },
+            )])),
+            body: Some(match result {
+                serde_json::Value::Null => "null".to_string(),
+                serde_json::Value::Bool(v) => v.to_string(),
+                serde_json::Value::Number(ref v) => format!("{}", v),
+                serde_json::Value::String(ref v) => v.to_owned(),
+                serde_json::Value::Array(_) => result.to_string(),
+                serde_json::Value::Object(_) => result.to_string(),
+            }),
         }
+    } else {
+        HandlerResult::default()
+    }
     Ok(())
 }
